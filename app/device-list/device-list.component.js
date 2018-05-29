2b504dfac8ee60e5585e86c3e15c210b498bc73d
alecoexposito <alecoexposito@gmail.com>
2018-05-29 17:12:39 -0400

    removing panOnOpen at labels snazzywindow

--- app/device-list/device-list.component.js
+++ app/device-list/device-list.component.js
@@ -386,7 +386,7 @@ angular.module('deviceList').component('deviceList', {
                 var m = self.findMarkerByImei(imei);
                 self.alarmMarker(m, alarmType, false);
                 if(alarmType == 100 || alarmType == '000') {
-                    var w = window.open(linkUrl, 'newwindow-' + alarm.device_id, 'width=1024,height=768');
+                    var w = window.open(linkUrl, 'newwindow-' + Date.now(), 'width=1024,height=768');
                     w.device = d;
                 }
             };
