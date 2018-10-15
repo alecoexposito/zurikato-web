4c07d425b9ecfaf1733344f0eebd20613f24cc18
alecoexposito <alecoexposito@gmail.com>
2018-10-15 15:03:44 -0400

    c5 socket ip

--- app/env.js
+++ app/env.js
@@ -18,8 +18,18 @@
 
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.apiUrl = 'http://187.217.220.34:3007/api/v1/';
-    window.__env.webUrl = 'http://187.217.220.34:8000/#!/';
-    window.__env.webSocketIp = '187.217.220.34';
+    window.__env.apiUrl = 'http://187.162.125.161:3007/api/v1/';
+    window.__env.webUrl = 'http://187.162.125.161:8000/#!/';
+    window.__env.webSocketIp = '187.162.125.161';
     window.__env.webSocketPort = 3001;
 }(this));
+
+// server dipepsa
+
+// (function (window) {
+//     window.__env = window.__env || {};
+//     window.__env.apiUrl = 'http://187.217.220.34:3007/api/v1/';
+//     window.__env.webUrl = 'http://187.217.220.34:8000/#!/';
+//     window.__env.webSocketIp = '187.217.220.34';
+//     window.__env.webSocketPort = 3001;
+// }(this));
