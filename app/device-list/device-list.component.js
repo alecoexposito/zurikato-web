0858a2a9850e4c0ec02e4dbddc3e7e04d9fa7620
alecoexposito <alecoexposito@gmail.com>
2018-10-30 15:21:29 -0400

    c5 socket ip

--- app/device-list/device-list.component.js
+++ app/device-list/device-list.component.js
@@ -1017,7 +1017,7 @@ angular.module('deviceList').component('deviceList', {
                             if(data.mdvr_number) {
                                 lastUpdate = data.date;
                                 var momentDate = moment("2018-10-30 13:35:26.0", "YYYY-MM-DD HH:mm:s.S")
-                                lastUpdate = momentDate.format(DD/MM/YYYY HH:mm:ss);
+                                lastUpdate = momentDate.format("DD/MM/YYYY HH:mm:ss");
                             } else {
                                 lastUpdate = self.getDateByHex(data.date);
                             }
