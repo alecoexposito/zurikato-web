d58c5fa4270863931f6c66137d73a488a1fa63aa
Alejandro <alecoexposito@gmail.com>
2019-05-16 10:28:58 -0400

    cambiando socket a c5

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/alert-c5', function(req, res) {
     //     // client.destroy();
     // });
 
-    client.connect(28, '187.162.125.161', function() {
+    client.connect(4105, '189.203.208.13', function() {
         console.log('Connected to socket');
         client.write(req.query.data);
         // client.destroy();
