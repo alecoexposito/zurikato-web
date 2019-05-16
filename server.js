57516f7897b42a572279e5e7c767491cdfe9f7ed
Alejandro <alecoexposito@gmail.com>
2019-05-16 12:03:39 -0400

    adding mdvr fix

--- server.js
+++ server.js
@@ -24,12 +24,14 @@ app.get('/api/alert-c5', function(req, res) {
     client.connect(28, '187.162.125.161', function() {
         console.log('Connected to socket');
         client.write(req.query.data);
+        // client.destroy();
     });
 
 
     // client.connect(4105, '189.203.208.13', function() {
     //     console.log('Connected to socket');
     //     client.write(req.query.data);
+    //     // client.destroy();
     // });
 
 
