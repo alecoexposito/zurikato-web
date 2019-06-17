3f1f4624fc3f8fc7b70fd1cb42bbfec4982c2d51
Alejandro <alecoexposito@gmail.com>
2019-06-17 00:06:09 -0400

    adding mdvr fix

---
+++ app/shared-video/shared-video.component.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('sharedVideo').component('sharedVideo', {
+    templateUrl: 'shared-video/shared-video.template.html',
+    controller: ['$http', '$timeout', '$routeParams',
+        function SharedScreenController($http, $timeout, $routeParams) {
+            var self = this;
+            self.currentIdDevice = null;
+            self.currentImei = null;
+            self.currentIdDevice = $routeParams.id;
+            // web sockets code
+            self.options = {
+                secure: false,
+                hostname: window.__env.webSocketIp,
+                port: window.__env.webSocketPort
+            };
+            self.socket = socketCluster.connect(self.options);
+            self.socket.on('connect', function () {
+                self.cameraChannel = self.socket.subscribe("camera_channel");
+                self.cameraVideoChannel = self.socket.subscribe("camera_" + self.currentIdDevice + "_channel");
+                self.cameraChannel.publish({ type: 'start-streaming', message: 'enviado desde la web', id: self.currentIdDevice });
+                self.cameraVideoChannel.watch(function(data) {
+                    console.log("camera video in: ", data);
+                    let base64Start = "data:image/jpeg;base64, ";
+                    var imgElem = document.getElementById("cameraImage");
+                    imgElem.setAttribute("src", base64Start + data.image);
+                });
+            });
+
+
+        }
+    ]
+});
---
+++ app/shared-video/shared-video.module.js
@@ -0,0 +1,5 @@
+'use strict';
+
+angular.module('sharedVideo', [
+    'core.device'
+]);
