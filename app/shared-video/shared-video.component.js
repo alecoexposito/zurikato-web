aa8d1f68429dcb8c2b9c6021d27a7628479e8ab0
Alejandro <alecoexposito@gmail.com>
2019-06-17 00:20:47 -0400

    adding mdvr fix

--- app/shared-video/shared-video.component.js
+++ app/shared-video/shared-video.component.js
@@ -20,7 +20,7 @@ angular.module('sharedVideo').component('sharedVideo', {
                 self.cameraVideoChannel = self.socket.subscribe("camera_" + self.currentIdDevice + "_channel");
                 self.cameraChannel.publish({ type: 'start-streaming', message: 'enviado desde la web', id: self.currentIdDevice });
                 self.cameraVideoChannel.watch(function(data) {
-                    console.log("camera video in: ", data);
+                    // console.log("camera video in: ", data);
                     let base64Start = "data:image/jpeg;base64, ";
                     var imgElem = document.getElementById("cameraImage");
                     imgElem.setAttribute("src", base64Start + data.image);
