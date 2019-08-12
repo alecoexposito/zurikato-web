3bf9dc097aafe84178eb3fdca319fcc390548e67
Alejandro <alecoexposito@gmail.com>
2019-08-12 11:08:23 -0400

    adding mdvr fix

--- app/device-list/device-list.component.js
+++ app/device-list/device-list.component.js
@@ -159,7 +159,7 @@ angular.module('deviceList').component('deviceList', {
                             }
                         });
 
-                    }, 3000);
+                    }, 10000);
 
                     self.playlistChannel = socket.subscribe(playlistName + '_channel');
                     self.playlistChannel.watch(function(data) {
